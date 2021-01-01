5ae11c56279c428357bee57f447702e1c1a7ad8a
ihebkotorsi <76847832+ihebkotorsi@users.noreply.github.com>
2021-01-01 19:07:38 +0100

    Update detailUser.js

--- src/Navigation/Tabs/Search/detailUser.js
+++ src/Navigation/Tabs/Search/detailUser.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import {TextInput, Button, Text, View, FlatList, Alert, TouchableOpacity } from 'react-native';
+import {StyleSheet,TextInput, Button, Text, View, FlatList, Alert, TouchableOpacity,Image } from 'react-native';
 import {useState} from "react";
 import Api from '../../../../services/dataService'
 import react, {Component} from 'react';
@@ -13,7 +13,20 @@ class AssoItem extends Component{
     render () {
       const {Asso} = this.props
       return (
-        <Text>{Asso}</Text>
+        <View style={styles.main_container}>
+          <Image
+            style={styles.image}
+            source={{uri:"image"}}
+          />
+          <View style={styles.content_container}>
+            <View style={styles.name_container}>
+              <Text style={styles.name}>{Asso.Nom}</Text>
+            </View>
+            <View style={styles.description_container}>
+              <Text style={styles.description}>{Asso.Description} </Text>
+            </View>
+          </View>
+        </View>
       )
   
     }
@@ -29,10 +42,20 @@ function UserDetail({route, navigation}){
 
     return (
       <View>
-        {console.log(route.params.item)}
-        <Text>Prenom : {route.params.item.Nom}</Text>
-        <Text>Nom : {route.params.item.Prenom}</Text>
-        <Text>Assos: </Text>
+        {console.log(route.params.item)}  
+        <View style={styles.main_container}>
+          <Image
+            style={styles.image}
+            source={{uri:"image"}}
+          />
+          <View style={styles.content_container}>
+            <Text style={styles.name}>Prenom : {route.params.item.Nom}</Text>
+            <Text style={styles.name}>Nom : {route.params.item.Prenom}</Text>
+          </View>
+        </View>
+        <View style={styles.title_container}>
+          <Text style={styles.name}>Les associations de {route.params.item.Nom} : </Text>
+        </View>
         <FlatList  
             data =  {route.params.item.ASSO}
             renderItem = { ( {item}) => <AssoItem  Asso = {item} /> }
@@ -44,5 +67,45 @@ function UserDetail({route, navigation}){
       </View>
     );
   }
-
+const styles=StyleSheet.create({
+  main_container:{
+    height:180,
+    flexDirection:'row'
+  },
+  image:{
+    height:170,
+    width:120,
+    margin:5,
+    backgroundColor:'gray'
+  },
+  content_container:{
+    flex:1,
+    margin:5
+  },
+  name_container:{
+    flex:2,
+    flexDirection:'row'
+  },
+  name:{
+    fontWeight:'bold',
+    fontSize:20,
+    flex:1,
+    flexWrap:'wrap',
+    paddingRight:5
+  },
+  description_container:{
+    flex:6
+  },
+  description:{
+    color:'#666666',
+    fontStyle:'italic'
+  },
+  text:{
+    fontWeight:'bold',
+    fontSize:20
+  },
+  title_container:{
+    height:30
+  }
+})
   export default UserDetail;
